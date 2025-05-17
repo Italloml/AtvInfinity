4c63bd380719591dadcf96018e014bdcdfa0a434
itallo <italoml@hotmail.com>
2025-05-17 10:26:40 -0300

    novas alterações

--- ProjetoInfinity/projeto/projeto_sabado/script.js
+++ ProjetoInfinity/projeto/projeto_sabado/script.js
@@ -1,22 +1,31 @@
 const listaTarefas = document.getElementById('lista-tarefas')
 const semTarefas = document.getElementById('sem-tarefas')
 const contadorTarefas = document.getElementById('contador-tarefas')
-const btnInfo = document.getElementById('btnCaptura')
+const btnAdd = document.getElementById('btn-adicionar')
 
-semTarefas.style.display = "none"
-
-btnInfo.addEventListener('click', (e) => {
+function excluirTarefas(e) {
+    listaTarefas.removeChild(e.target.parentElement)
+    const tamanhoUL = listaTarefas.children.length
+    if(tamanhoUL === 1) {
+        semTarefas.style.display = 'none'
+    }
+}
+btnAdd.addEventListener('click', (e) => {
+    semTarefas.style.display = 'none'
     e.preventDefault()
     const novaTarefa = document.createElement('li')
+    novaTarefa.className = 'tarefa'
     novaTarefa.innerHTML = `
-        <input type="checkbox">
-        <span class="tarefa-Texto">Nova Tarefa</span>
-        <button>Excluir</button>
-`
+        <input type="checkbox" class="checkbox-tarefa">
+        <span class="tarefa-Texto">${input.value}{</span>
+        <button onclic=excluirTarefas(event) class="btn-excluir">Excluir</button>
+    `
     listaTarefas.appendChild(novaTarefa)
-    
+    input.value = ''
+    input.focus()
 })
-
-
+console.dir(input)
 
 contadorTarefas.innerText = `Total de Tarefas: ${listaTarefas.children.length - 1}`
+
+// incompleto
